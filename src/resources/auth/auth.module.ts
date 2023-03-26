74c92004c37fd22fc7e48dd9c4861334dd5edd75
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-26 18:44:25 +0530

    feat: access token support

--- src/resources/auth/auth.module.ts
+++ src/resources/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
