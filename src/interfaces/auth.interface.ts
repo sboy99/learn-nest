da6c9b0a9368cf4ad55c447c96c027a954e5dfe6
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-26 18:31:28 +0530

    chore: updated auth interface

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -1,6 +1,5 @@
-import { User } from '@prisma/client';
 import { IResMessage } from './global.interface';
 
 export interface IAuthRes extends IResMessage {
-  user: Omit<User, 'password'>;
+  access_token: string;
 }
