1f2915cf0aae7d09c8a6a6cc668e73fd9abab446
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-26 11:28:37 +0530

    feat: inatalled class validator & morgan

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  // morgan
+  app.use(morgan('dev'));
+
+  const port = process.env.PORT || 8000;
+  await app.listen(port);
+  console.log(`Server started on port ${port}`);
 }
 bootstrap();
