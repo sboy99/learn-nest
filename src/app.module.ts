2c6158e7a74878822e32c98e464bb195cd31ae91
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-26 11:21:18 +0530

    chore: updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './resources/auth/auth.module';
+import { BookmarkModule } from './resources/bookmark/bookmark.module';
+import { UserModule } from './resources/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    DatabaseModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
