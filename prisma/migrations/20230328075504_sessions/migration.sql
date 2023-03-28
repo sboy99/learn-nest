cf9ee498152757b378328d9e3bd2bf7a1d7504f7
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-28 13:37:28 +0530

    feat(db): added sessions model

---
+++ prisma/migrations/20230328075504_sessions/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" SERIAL NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "refresh_token" TEXT NOT NULL,
+    "is_blocked" BOOLEAN NOT NULL DEFAULT false,
+    "ip" TEXT NOT NULL,
+    "user_agent" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_user_id_key" ON "sessions"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
