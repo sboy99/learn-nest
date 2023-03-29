9cc5f01fb6127702c7b82d91bf11e10f6645cc4c
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-29 16:08:18 +0530

    chore: updated auth dtos

--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -1,13 +1,42 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from 'class-validator';
 
-export class AuthDto {
+export class SignupDto {
   @IsNotEmpty()
+  @IsString()
   username: string;
 
   @IsNotEmpty()
+  @IsString()
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
+  @IsString()
+  @IsStrongPassword({
+    minLength:6,
+    minLowercase:0,
+    minNumbers:0,
+    minSymbols:0,
+    minUppercase:0
+  })
   password: string;
 }
+
+export class SigninDto {
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsStrongPassword({
+    minLength:6,
+    minLowercase:0,
+    minNumbers:0,
+    minSymbols:0,
+    minUppercase:0
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
