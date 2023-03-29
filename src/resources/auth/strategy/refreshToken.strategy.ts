dbf6eece53847bbd7230bee59998e0331a1ab1d5
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-29 16:09:15 +0530

    chore: updated strategies

---
+++ src/resources/auth/strategy/refreshToken.strategy.ts
@@ -0,0 +1,27 @@
+import { Token } from '@/decorators';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'check-refresh-token',
+) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  validate(@Token() refreshToken: string, payload: any) {
+    console.log(payload);
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
