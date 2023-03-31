3f73aff4461f01eeede8261db59571010298bb90
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-31 15:38:33 +0530

    chore: added jwt user interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,5 @@
 export * from './auth.interface';
 export * from './global.interface';
+export * from './jwtUser.interface';
 export * from './reqInfo.interface';
 export * from './tokens.interface';
