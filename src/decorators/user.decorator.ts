561e4d7803a81bb8999e72142dcf15e87d663bc9
Sagar Bera <contact.sagarbera@gmail.com>
2023-03-31 15:40:05 +0530

    chore: updated user decorator

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,8 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 
 export const User = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest() as Request;
+    if (!data) return request.user;
+    return request.user[data];
   },
 );
