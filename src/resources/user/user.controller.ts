ea7037bb29a4a0e4231ff535ccdd356898dead48
Sagar Bera <contact.sagarbera@gmail.com>
2023-04-02 13:10:03 +0530

    feat: implement all & single user fetch

--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { User } from '@/decorators';
+import { UseParam, User } from '@/decorators';
+import { IRes } from '@/interfaces';
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User as TUser } from '@prisma/client';
 import { AccessTokenGuard } from '../auth/guards';
@@ -8,9 +9,40 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get('/')
+  async getAllUsers(): Promise<
+    | (IRes<Pick<TUser, 'id' | 'email' | 'username' | 'created_at'>[]> & {
+        count: number;
+      })
+    | never
+  > {
+    const users = await this.userService.getAllUsers();
+    return {
+      code: 'SUCCESS',
+      message: `successfully found ${users.length} users`,
+      count: users.length,
+      data: users,
+    };
+  }
+
+  @Get('/:userId')
+  async getUser(
+    @UseParam('userId') userId: string,
+  ): Promise<IRes<Omit<TUser, 'password'>> | never> {
+    const user = await this.userService.getUser(userId);
+    delete user.password;
+    return {
+      code: 'SUCCESS',
+      message: 'Success',
+      data: user,
+    };
+  }
+
   @Get('me')
   @UseGuards(AccessTokenGuard)
-  async showUser(@User() user: TUser) {
-    return this.userService.handleShowUser(user);
+  async showUser(
+    @User('userId') userId: string,
+  ): Promise<Pick<TUser, 'id' | 'email' | 'username'> | never> {
+    return this.userService.handleShowUser(userId);
   }
 }
--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -1,9 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '@/database/database.service';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { User as TUser } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-  handleShowUser(user: TUser) {
+  constructor(private readonly db: DatabaseService) {}
+  /**
+   *
+   * @param userId string
+   * @returns {
+   * id: string;
+   * username: string;
+   * email: string;
+   * }
+   */
+  async handleShowUser(
+    userId: string,
+  ): Promise<Pick<TUser, 'id' | 'email' | 'username'> | never> {
+    const user = await this.db.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+      },
+    });
+    if (!user)
+      throw new UnauthorizedException(
+        'You are unauthorized to access this route',
+      );
+    return user;
+  }
+
+  async getAllUsers(): Promise<
+    Pick<TUser, 'id' | 'email' | 'username' | 'created_at'>[] | never
+  > {
+    const users = await this.db.user.findMany({
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        created_at: true,
+      },
+    });
+    return users;
+  }
+
+  async getUser(userId: string): Promise<TUser | never> {
+    if (!userId || userId === ':userId')
+      throw new BadRequestException('userId should not be empty');
+    const user = await this.db.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
     return user;
   }
 }
