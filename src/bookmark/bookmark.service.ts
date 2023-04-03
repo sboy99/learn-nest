2a44b7e00d49c6027d4e2c932226ba69441eba2c
Sagar Bera <contact.sagarbera@gmail.com>
2023-04-03 16:21:15 +0530

    feat: implmented business logic for bookmark creation

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,11 +1,27 @@
+import { DatabaseService } from '@/database/database.service';
+import { IJwtUser } from '@/interfaces';
 import { Injectable } from '@nestjs/common';
+import { Bookmark } from '@prisma/client';
 import { CreateBookmarkDto } from './dto/create-bookmark.dto';
 import { UpdateBookmarkDto } from './dto/update-bookmark.dto';
 
 @Injectable()
 export class BookmarkService {
-  create(createBookmarkDto: CreateBookmarkDto) {
-    return 'This action adds a new bookmark';
+  constructor(private readonly db: DatabaseService) {}
+  async create(
+    createBookmarkDto: CreateBookmarkDto,
+    userId: IJwtUser['userId'],
+  ): Promise<Bookmark | never> {
+    const bookmark = await this.db.bookmark.create({
+      data: {
+        user_id: userId,
+        title: createBookmarkDto.title,
+        description: createBookmarkDto.description,
+        url: createBookmarkDto.url,
+      },
+    });
+
+    return bookmark;
   }
 
   findAll() {
