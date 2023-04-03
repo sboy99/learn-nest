91d54fb984f32394b918249ed61a468c3571bb44
Sagar Bera <contact.sagarbera@gmail.com>
2023-04-03 16:17:58 +0530

    chore: declred dto for book management

--- src/bookmark/dto/create-bookmark.dto.ts
+++ src/bookmark/dto/create-bookmark.dto.ts
@@ -1,1 +1,33 @@
-export class CreateBookmarkDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateBookmarkDto {
+  @MinLength(2, {
+    message: 'Title is too short',
+  })
+  @MaxLength(50, {
+    message: 'Title is too long',
+  })
+  @IsString({
+    message: 'Title should be a string',
+  })
+  @IsNotEmpty({
+    message: 'Title should not be empty',
+  })
+  title: string;
+
+  @MinLength(15, {
+    message: 'Description should have atleast 15 chars',
+  })
+  @IsString({
+    message: 'Description should be a string',
+  })
+  @IsNotEmpty({
+    message: 'Description should not be empty',
+  })
+  description: string;
+
+  @IsString({
+    message: 'URl should be a string',
+  })
+  url?: string;
+}
